3402774a4515480c1e99a9523c7900e8e50a02f8
Milena Baeza <milena@lovelystay.com>
2019-12-09 10:43:09 +0000

    Add images to art&photography section

--- src/components/ArtGallery.vue
+++ src/components/ArtGallery.vue
@@ -1,65 +1,64 @@
 <template>
   <div class="c-gallery">
-    <VueTinySlider
-      :items="1" :mode="'gallery'" :mouse-drag="true"
-      :arrow-keys="true"
-    >
-        <div class="slide">
-            <img src="../assets/nenufares.jpg" alt="">
-        </div>
-        <div class="slide">
-            <img src="../assets/nenufares-02.jpg" alt="">
-        </div>
-        <div class="slide">
-            <img src="../assets/journal-green.jpg" alt="">
-        </div>
-        <div class="slide">
-            <img src="../assets/aquarium.jpg" alt="Photo of an artificial lake with fishes">
-        </div>
-        <div class="slide">
-            <img src="../assets/lady-flower.jpg" alt="Collage of a woman's face covered with flowers">
-        </div>
-        <div class="slide">
-            <img src="../assets/beach-black-yellow.jpg" alt="">
-        </div>
-        <div class="slide">
-            <img src="../assets/beach-blue-yellow.jpg" alt="">
-        </div>
-        <div class="slide">
-            <img src="../assets/journal-red.jpg" alt="">
-        </div>
-        <div class="slide">
-            <img src="../assets/journal-yellow.jpg" alt="">
-        </div>
-        <div class="slide">
-            <img src="../assets/journal-drawing-01.jpg" alt="">
-        </div>
-        <div class="slide">
-            <img src="../assets/sea-sand-sunset.jpg" alt="">
-        </div>
-        <div class="slide">
-            <img src="../assets/sea-sunset.jpg" alt="">
-        </div>
-        <div class="slide">
-            <img src="../assets/journal-drawing-02.jpg" alt="">
-        </div>
-        <div class="slide">
-            <img src="../assets/journal-france.jpg" alt="">
-        </div>
-        <div class="slide">
-            <img src="../assets/beach-blue-yellow-02.jpg" alt="">
-        </div>
-    </VueTinySlider>
-    <p class="u-paragraph u-hide--desktop">[ swipe ]</p>
+    <div>
+        <img src="../assets/cactus.jpg" alt="">
+    </div>
+    <div>
+        <img src="../assets/lady-flower.jpg" alt="Collage of a woman's face covered with flowers">
+    </div>
+    <div>
+        <img src="../assets/nenufares.jpg" alt="">
+    </div>
+    <div>
+        <img src="../assets/nenufares-02.jpg" alt="">
+    </div>
+    <div>
+        <img src="../assets/journal-green.jpg" alt="">
+    </div>
+    <div>
+        <img src="../assets/aquarium.jpg" alt="Photo of an artificial lake with fishes">
+    </div>
+    <div>
+        <img src="../assets/beach-black-yellow.jpg" alt="">
+    </div>
+    <div>
+        <img src="../assets/beach-blue-yellow.jpg" alt="">
+    </div>
+    <div>
+        <img src="../assets/beach-blue-yellow-02.jpg" alt="">
+    </div>
+    <div>
+        <img src="../assets/journal-red.jpg" alt="">
+    </div>
+    <div>
+        <img src="../assets/journal-yellow.jpg" alt="">
+    </div>
+    <div>
+        <img src="../assets/journal-drawing-01.jpg" alt="">
+    </div>
+    <div>
+        <img src="../assets/sea-sand-sunset.jpg" alt="">
+    </div>
+    <div>
+        <img src="../assets/sea-sunset.jpg" alt="">
+    </div>
+    <div>
+        <img src="../assets/collage-card.jpg" alt="">
+    </div>
+    <div>
+        <img src="../assets/journal-drawing-02.jpg" alt="">
+    </div>
+    <div>
+        <img src="../assets/journal-france.jpg" alt="">
+    </div>
+    <div>
+        <img src="../assets/collage-lady-sleeping.jpg" alt="">
+    </div>
+    <div>
+        <img src="../assets/journal-drawing-03.jpg" alt="">
+    </div>
+    <div>
+        <img src="../assets/journal-orange.jpg" alt="">
+    </div>
   </div>
 </template>
-
-<script>
-import VueTinySlider from 'vue-tiny-slider';
-export default {
-  name: 'ArtGallery',
-  components: {
-    VueTinySlider
-  }
-}
-</script>
